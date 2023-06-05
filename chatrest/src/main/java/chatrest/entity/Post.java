e4a20c3c5d8c2ba79c9a720f9e4961e60e3b1bf4
Hsabonchi <hsadeveloper@gmail.com>
2023-06-04 17:54:11 -0700

    refresh files

--- chatrest/src/main/java/chatrest/entity/Post.java
+++ chatrest/src/main/java/chatrest/entity/Post.java
@@ -2,16 +2,12 @@ package chatrest.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
@@ -45,8 +41,8 @@ public class Post implements Serializable {
    * One post can have zero OR one medeiaFile One mediaFile belong to one or more posts. 1 - M
    */
 
-  // @OneToMany
-  // private Set<MediaFile> mediaFile = new HashSet();;
+  @OneToMany
+   private Set<MediaFile> mediaFile = new HashSet();;
 
   /*
    * One post can have zero OR one parent One parent can be a parent of multiple posts.
