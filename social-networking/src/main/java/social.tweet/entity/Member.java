d947ecc3bec8ddc8a263b3999b6b7e001930d5ba
Hsabonchi <hsadeveloper@gmail.com>
2023-06-25 15:37:44 -0700

    create Room entity and change package name

--- social-networking/src/main/java/social.tweet/entity/Member.java
+++ social-networking/src/main/java/social.tweet/entity/Member.java
@@ -1,6 +1,5 @@
 package demomongodb.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
